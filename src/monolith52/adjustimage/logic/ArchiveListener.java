8bb200796c37da04e0d99af01a54e5e9b5bed231
Monolith <Monolith@192.168.2.100>
2017-03-06 13:47:57 +0900

    concurrentをマージ

--- src/monolith52/adjustimage/logic/ArchiveListener.java
+++ src/monolith52/adjustimage/logic/ArchiveListener.java
@@ -5,7 +5,7 @@ import java.io.File;
 public interface ArchiveListener {
 
 	public void update(File inputFile, Progress progress);
-	public void success(File inputFile, File outputFile);
+	public void success(File inputFile, long outputFilesize);
 	public void failed(File inputFile, String msg);
 	
 }
